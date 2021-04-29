65dd30fc29f5167f222194de02f9bd5ed6e3cd4d
Austin <aturn9880@gmail.com>
2021-04-29 14:57:42 -0400

    commit tree c504e9065807d1e886e244f9386bb83efe76f606

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
-// Write your code here
+const num1 = 31
+const num2 = 2
+let multiply = num1 * num2;
+
+const random = Math.floor(Math.random() + 2);
+// console.log(random);
+
+const num3 = 16
+const num4 = 4
+let mod = num3 / num4;
+
+const max = Math.max(1, 5, 10, 20);
